5c1aade83613eaa4b120af479e07750217d3a004
Alex <avlonitis@msn.com>
2020-03-02 17:26:26 +0000

    Display raw image data

--- api/handlers.go
+++ api/handlers.go
@@ -8,10 +8,15 @@ import (
 	"github.com/docker/docker/api/types"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func imageList(w http.ResponseWriter, r *http.Request) {
 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	dieIfErr(err)
 
+	enableCors(&w)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(images)
 }
@@ -20,6 +25,7 @@ func containerList(w http.ResponseWriter, r *http.Request) {
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	dieIfErr(err)
 
+	enableCors(&w)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(containers)
 }
--- frontend/src/components/ImageList.vue
+++ frontend/src/components/ImageList.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
     <h1>Image List</h1>
+    {{ info }}
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
-    name: 'ImageList'
+    name: 'ImageList',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('http://localhost:1234/images')
+        .then(response => (this.info = response))
+    }
   }
 </script>
